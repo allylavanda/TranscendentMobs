d68d646118980ee6de87fea50c748a885ef097b9
Danni <sjaeledyr@gmail.com>
2021-06-29 18:43:54 -0400

    Add files via upload

---
+++ src/main/java/me/sjaeledyr/transcendentmobs/Main.java
@@ -0,0 +1,28 @@
+package me.sjaeledyr.transcendentmobs;
+
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Main extends JavaPlugin {
+    public static FileConfiguration config;
+    @Override
+    public void onEnable() {
+        config=getConfig();
+        this.saveDefaultConfig();
+        // Plugin startup logic
+        System.out.println("[TranscendentMobs] TranscendentMobs by Sjaeledyr has been Loaded! ");
+        System.out.println("Plugin Version 1.0");
+
+        // Set up event listener
+        getServer().getPluginManager().registerEvents(new SpigotListener(), this);
+        config.set("test", true);
+    }
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+        System.out.println("[TranscendentMobs] TranscendentMobs has been disabled!");
+        config = null;
+    }
+
+
+}
---
+++ src/main/java/me/sjaeledyr/transcendentmobs/Mobs/ZombieWarrior.java
@@ -0,0 +1,4 @@
+package me.sjaeledyr.transcendentmobs.Mobs;
+
+public class ZombieWarrior {
+}
