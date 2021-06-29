ed989ada8a40d5fea5735ecbb78e03213499f7bc
Danni <sjaeledyr@gmail.com>
2021-06-29 19:59:24 -0400

    Add files via upload

---
+++ src/main/java/me/sjaeledyr/transcendentmobs/Mobs/ZombieWarrior.java
@@ -0,0 +1,39 @@
+package me.sjaeledyr.transcendentmobs.Mobs;
+
+import me.sjaeledyr.transcendentmobs.util.killCounter;
+import org.bukkit.Location;
+import org.bukkit.Material;
+import org.bukkit.World;
+import org.bukkit.entity.Entity;
+import org.bukkit.entity.EntityType;
+import org.bukkit.entity.Player;
+import org.bukkit.entity.Zombie;
+import org.bukkit.event.entity.EntityDeathEvent;
+import org.bukkit.inventory.ItemStack;
+
+public class ZombieWarrior {
+    killCounter kc = new killCounter();
+
+    public void zombieDeathHandler(EntityDeathEvent e) {
+        Entity entity = e.getEntity();
+        Player p = e.getEntity().getKiller();
+        kc.addKillZombie(p, 1);
+        // Zombie Warrior Spawn Condition
+        if (kc.getKillsZombie(p) >= 3) {
+            // SpawnLoc
+            Location spawnLoc = p.getLocation().add(2,0,0);
+            World world = p.getWorld();
+            p.sendMessage("A Zombie Warrior Has Appeared!");
+            Zombie zombie = (Zombie) world.spawnEntity(spawnLoc, EntityType.ZOMBIE);
+            zombie.setCustomName("Zombie Warrior");
+            zombie.setCustomNameVisible(true);
+            // Zombie Warrior Gear
+            zombie.getEquipment().setItemInMainHand(new ItemStack(Material.IRON_AXE));
+            zombie.getEquipment().setHelmet(new ItemStack(Material.DIAMOND_HELMET));
+            zombie.getEquipment().setChestplate(new ItemStack(Material.IRON_CHESTPLATE));
+            zombie.getEquipment().setLeggings(new ItemStack(Material.IRON_LEGGINGS));
+            zombie.getEquipment().setBoots(new ItemStack(Material.GOLDEN_BOOTS));
+            kc.resetKillsZombie(p);
+        }
+    }
+}
---
+++ src/main/java/me/sjaeledyr/transcendentmobs/util/killCounter.java
@@ -0,0 +1,37 @@
+package me.sjaeledyr.transcendentmobs.util;
+
+import me.sjaeledyr.transcendentmobs.Main;
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.entity.Player;
+
+public class killCounter {
+    private Main main = Main.getPlugin(Main.class);
+    FileConfiguration cfg = main.getConfig();
+
+    public void addKillSkeleton(Player p, int count) {
+        int i = cfg.getInt(p.getName() + "." + "skeleton kills");
+        int a = count;
+        cfg.set(p.getName() + "." + "skeleton kills", a + i);
+        main.saveConfig();
+    }
+    public int getKillsSkeleton(Player p) {
+        return cfg.getInt(p.getName() + "." + "skeleton kills");
+    }
+    public void resetKillsSkeleton(Player p) {
+        cfg.set(p.getName() + "." + "skeleton kills", 0);
+        main.saveConfig();
+    }
+    public void addKillZombie(Player p, int count) {
+        int i = cfg.getInt(p.getName() + "." + "zombie kills");
+        int a = count;
+        cfg.set(p.getName() + "." + "zombie kills", a + i);
+        main.saveConfig();
+    }
+    public int getKillsZombie(Player p) {
+        return cfg.getInt(p.getName() + "." + "zombie kills");
+    }
+    public void resetKillsZombie(Player p) {
+        cfg.set(p.getName() + "." + "zombie kills", 0);
+        main.saveConfig();
+    }
+}
