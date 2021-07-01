b632f82e651f89abb3cdbf43fab661496d08b76c
Danni <sjaeledyr@gmail.com>
2021-07-01 16:53:55 -0400

    attributable weapon

--- src/main/java/me/sjaeledyr/transcendentmobs/Mobs/SkeletonCrusader.java
+++ src/main/java/me/sjaeledyr/transcendentmobs/Mobs/SkeletonCrusader.java
@@ -2,15 +2,17 @@ package me.sjaeledyr.transcendentmobs.Mobs;
 
 import org.bukkit.*;
 import org.bukkit.attribute.Attribute;
+import org.bukkit.attribute.AttributeModifier;
 import org.bukkit.entity.Entity;
 import org.bukkit.entity.EntityType;
 import org.bukkit.entity.Player;
 import org.bukkit.entity.Skeleton;
 import org.bukkit.event.entity.EntityDeathEvent;
+import org.bukkit.inventory.EquipmentSlot;
 import org.bukkit.inventory.ItemStack;
 import me.sjaeledyr.transcendentmobs.util.killCounter;
-import org.bukkit.potion.PotionEffect;
-import org.bukkit.potion.PotionEffectType;
+import org.bukkit.inventory.meta.ItemMeta;
+import java.util.UUID;
 
 public class SkeletonCrusader {
     killCounter kc = new killCounter();
@@ -28,19 +30,26 @@ public class SkeletonCrusader {
             Skeleton skeleton = (Skeleton) world.spawnEntity(spawnLoc, EntityType.SKELETON);
             skeleton.setCustomName("Skeleton Crusader");
             skeleton.setCustomNameVisible(true);
-            // Attributes
-            skeleton.addPotionEffect(new PotionEffect(PotionEffectType.INCREASE_DAMAGE, 300000000, 1));
             skeleton.getAttribute(Attribute.GENERIC_MAX_HEALTH).setBaseValue(40);
             skeleton.setHealth(40);
             //Particle Effects
             world.spawnParticle(Particle.EXPLOSION_HUGE, skeleton.getLocation(), 1);
+
             // Skeleton Crusader Gear
-            skeleton.getEquipment().setItemInMainHand(new ItemStack(Material.DIAMOND_AXE));
+            AttributeModifier modifier = new AttributeModifier(UUID.randomUUID(), "generic.attackDamage", 100,
+                    AttributeModifier.Operation.ADD_NUMBER, EquipmentSlot.HAND);
+            ItemStack scWeapon = new ItemStack(Material.DIAMOND_AXE);
+            ItemMeta meta = scWeapon.getItemMeta();
+            // add a modifier
+            meta.addAttributeModifier(Attribute.GENERIC_ATTACK_DAMAGE, modifier);
+            scWeapon.setItemMeta(meta);
+            kc.resetKillsSkeleton(p);
+
+            skeleton.getEquipment().setItemInMainHand(scWeapon);
             skeleton.getEquipment().setHelmet(new ItemStack(Material.GOLDEN_HELMET));
             skeleton.getEquipment().setChestplate(new ItemStack(Material.GOLDEN_CHESTPLATE));
             skeleton.getEquipment().setLeggings(new ItemStack(Material.GOLDEN_LEGGINGS));
             skeleton.getEquipment().setBoots(new ItemStack(Material.GOLDEN_BOOTS));
-            kc.resetKillsSkeleton(p);
         }
     }
 }
