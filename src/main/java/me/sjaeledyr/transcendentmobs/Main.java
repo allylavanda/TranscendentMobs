f3cda4fdfa58715bb586868586e5cfa51d5b44c2
Danni <sjaeledyr@gmail.com>
2021-07-23 18:28:35 -0400

    Update Main.java

--- src/main/java/me/sjaeledyr/transcendentmobs/Main.java
+++ src/main/java/me/sjaeledyr/transcendentmobs/Main.java
@@ -2,20 +2,29 @@ package me.sjaeledyr.transcendentmobs;
 
 import me.sjaeledyr.transcendentmobs.Loot.Armor;
 import me.sjaeledyr.transcendentmobs.Loot.Weapons;
+import me.sjaeledyr.transcendentmobs.util.AugmentGUI;
 import me.sjaeledyr.transcendentmobs.util.EventListener;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
 import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class Main extends JavaPlugin {
     public static FileConfiguration config;
     private final Weapons weapons = new Weapons();
     private final Armor armor = new Armor();
+    private final AugmentGUI aGUI = new AugmentGUI();
 
     @Override
-    public final void onEnable() {
+    public final void onEnable()
+    {
+        // Commands
+        this.getCommand("augment").setExecutor(new AugmentGUI());
+
+        // Plugin startup logic
         config=getConfig();
         this.saveDefaultConfig();
-        // Plugin startup logic
         weapons.init();
         armor.init();
         System.out.println("[TranscendentMobs] TranscendentMobs by Sjaeledyr has been Loaded! ");
@@ -26,11 +35,22 @@ public class Main extends JavaPlugin {
         config.set("test", true);
     }
     @Override
-    public void onDisable() {
+    public void onDisable()
+    {
         // Plugin shutdown logic
         System.out.println("[TranscendentMobs] TranscendentMobs has been disabled!");
         config = null;
     }
-
-
+    public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args)
+    {
+        Player p = (Player) sender;
+        if (label.equalsIgnoreCase("augment")) {
+            if (!(sender instanceof Player)) {
+                sender.sendMessage("You cannot do this!");
+                return true;
+            }
+        }
+        aGUI.createInv(p);
+        return true;
+    }
 }
