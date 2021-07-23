5d1ae7ad629042fc331bc23b23a008697d55a5cc
Danni <sjaeledyr@gmail.com>
2021-07-23 18:28:19 -0400

    Update AugmentGUI.java

--- src/main/java/me/sjaeledyr/transcendentmobs/util/AugmentGUI.java
+++ src/main/java/me/sjaeledyr/transcendentmobs/util/AugmentGUI.java
@@ -3,15 +3,13 @@ package me.sjaeledyr.transcendentmobs.util;
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
-import org.bukkit.entity.Item;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.ItemStack;
-import org.bukkit.inventory.PlayerInventory;
 
-public class AugmentGUI {
-
+public class AugmentGUI implements CommandExecutor {
     public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args)
     {
         Player p = (Player) sender;
@@ -21,15 +19,19 @@ public class AugmentGUI {
                 return true;
             }
         }
-        return false;
+        createInv(p);
+        sender.sendMessage("Debug");
+        return true;
     }
-
-    private void createInv(final Player player)
+    public void createInv(final Player player)
     {
-        Inventory inv = Bukkit.createInventory(null, 9, ChatColor.GOLD + "" + ChatColor.BOLD + "Weapon Augmentation");
+        Inventory inv = Bukkit.createInventory(null, 9, ChatColor.DARK_BLUE + "" + ChatColor.BOLD + "Weapon Augmentation");
         ItemStack item = player.getInventory().getItemInMainHand();
-        if (item == null)
+        inv.setItem(0, item);
+        if (item == null) {
+            player.sendMessage("You must have an augmentable item equipped!");
             return;
-
+        }
+        player.openInventory(inv);
     }
 }
